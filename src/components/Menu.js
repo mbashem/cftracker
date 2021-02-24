49f6543ae7af2d244410e515efb8c95be32a63c9
Mohammed Bashem <45259719+mbashem@users.noreply.github.com>
2021-02-24 16:44:42 +0600

    audit fixed

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const PROBLEMS = "/problems";
 export const CONTEST = "/contests";
