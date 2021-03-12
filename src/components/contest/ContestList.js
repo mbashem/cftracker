cb4f1c2fdbefe16b505c3c3c3be805fd14ff0ab1
Mohammed Bashem <45259719+mbashem@users.noreply.github.com>
2021-03-12 19:49:32 +0600

    Tested Shared Problems

--- src/components/contest/ContestList.js
+++ src/components/contest/ContestList.js
@@ -1,3 +1,4 @@
+import { render } from "@testing-library/react";
 import React from "react";
 import { useSelector } from "react-redux";
 import {
@@ -11,77 +12,59 @@ import { ATTEMPTED_PROBLEMS, SOLVED_PROBLEMS } from "../../util/constants";
 const ContestList = (props) => {
   const state = useSelector((state) => state);
 
-  const getInfo = (contestId, index) => {
+  const related = [
+    {
+      contestId: 1464,
+      index: "A",
+      shared: [
+        { contestId: 1411, index: "C" },
+        { contestId: 1465, index: "C" },
+      ],
+    },
+    {
+      contestId: 1465,
+      index: "C",
+      shared: [
+        { contestId: 1411, index: "C" },
+        { contestId: 1464, index: "A" },
+      ],
+    },
+    {
+      contestId: 1496,
+      index: "C",
+      shared: [{ contestId: 1495, index: "A" }],
+    },
+  ];
+
+  const getProblem = (contestId, index) => {
     let l = 0,
-      r = state.problemList.problems.length - 1,
-      ans = -1;
+      r = state.problemList.problems.length - 1;
     while (l <= r) {
       let mid = l + ((r - l) >> 2);
       if (
         state.problemList.problems[mid].contestId === contestId &&
         state.problemList.problems[mid].index === index
-      ) {
-        ans = mid;
-        break;
-      }
+      )
+        return state.problemList.problems[mid];
 
       if (
         state.problemList.problems[mid].contestId > contestId ||
         (state.problemList.problems[mid].contestId === contestId &&
           state.problemList.problems[mid].index > index)
-      ) {
+      )
         r = mid - 1;
-      } else l = mid + 1;
+      else l = mid + 1;
     }
 
-    const EMPTY = "EMPTY bg-dark";
+    return -1;
+  };
 
-    if (ans === -1 && index.length !== 1)
-      return <td key={contestId + index} className={EMPTY}></td>;
+  const renderProblem = (problem) => {
+    let solved = state.userSubmissions[SOLVED_PROBLEMS].has(problem.id);
+    let attempted = state.userSubmissions[ATTEMPTED_PROBLEMS].has(problem.id);
 
-    if (ans === -1 && index.length === 1) {
-      let arr = [];
-      for (let i = 1; ; i++) {
-        let res = getInfo(contestId, index + i);
-        if (res.props.className === EMPTY) break;
-        arr.push(res);
-      }
-
-      if (arr.length == 0) {
-        return <td key={contestId + index} className={EMPTY}></td>;
-      }
-
-      if (arr.length < 3)
-        return (
-          <td className="p-0" key={contestId + index.charAt(0)}>
-            <table>
-              <tbody>
-                <tr>
-                  <td className="inside p-0" key={contestId + index}>
-                    {arr.map((element) => element)}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </td>
-        );
-      else
-        return (
-          <td className="inside p-0" key={contestId + index}>
-            More than 4
-          </td>
-        );
-    }
-
-    let solved = state.userSubmissions[SOLVED_PROBLEMS].has(
-      contestId.toString() + index
-    );
-    let attempted = state.userSubmissions[ATTEMPTED_PROBLEMS].has(
-      contestId + index
-    );
-
-    let name = state.problemList.problems[ans].name;
-    let id = state.problemList.problems[ans].id;
+    let name = problem.name;
+    let id = problem.id;
     if (name.length > 10) name = name.substring(0, 9) + "...";
 
     let className =
@@ -95,20 +78,109 @@ const ContestList = (props) => {
           rel="noreferrer"
           tabIndex="0"
           data-bs-toggle="tooltip"
-          title={
-            state.problemList.problems[ans].name +
-            ", Rating:" +
-            state.problemList.problems[ans].rating
-          }
-          href={getProblemUrl(contestId, index)}>
-          {index + ". "}
+          title={problem.name + ", Rating:" + problem.rating}
+          href={getProblemUrl(problem.contestId, problem.index)}>
+          {problem.index + ". "}
           {name}
         </a>
       </td>
     );
   };
 
-  const renderProblem = () => {};
+  const getSharedIndex = (contestId, index) => {
+    let l = 0,
+      r = related.length - 1;
+
+    while (l <= r) {
+      let mid = l + ((r - l) >> 2);
+      if (related[mid].contestId === contestId && related[mid].index === index)
+        return mid;
+      if (
+        related[mid].contestId > contestId ||
+        (related[mid].contestId === contestId && related[mid].index > index)
+      )
+        r = mid - 1;
+      else l = mid + 1;
+    }
+
+    return -1;
+  };
+
+  const getProblemsList = (contestId, index, first = true) => {
+    let problem = getProblem(contestId, index);
+
+    let problems = [];
+    if (problem === -1) {
+      let problem1 = getProblem(contestId, index + "1");
+      if (problem1 === -1 && first === true) {
+        let sharedIndex = getSharedIndex(contestId, index);
+        if (sharedIndex != -1) {
+          for (let sharedProblem of related[sharedIndex].shared) {
+            let currentGetInfo = getProblemsList(
+              sharedProblem.contestId,
+              sharedProblem.index,
+              false
+            );
+
+            if (currentGetInfo.length === 1 && currentGetInfo[0] === -1)
+              continue;
+            for (let currentProblem of currentGetInfo) {
+              let current = { ...currentProblem };
+              current.contestId = contestId;
+              current.index = current.index.split("");
+              current.index[0] = index.charAt(0);
+              current.index = current.index.join("");
+              current.id = current.contestId.toString() + index;
+              problems.push(current);
+            }
+          }
+        }
+      } else {
+        problems.push(problem1);
+        for (let c = "2"; c <= "3"; c++) {
+          problem1 = getProblem(contestId, index + c);
+          if (problem1 === -1) break;
+          problems.push(problem1);
+        }
+      }
+    } else problems.push(problem);
+
+    return problems;
+  };
+
+  const getInfo = (contestId, index) => {
+    const EMPTY = "EMPTY bg-dark";
+
+    let problems = getProblemsList(contestId, index);
+
+    if (problems.length === 0) {
+      return <td key={contestId + index} className={EMPTY}></td>;
+    }
+
+    if (problems.length === 1) {
+      return renderProblem(problems[0]);
+    }
+
+    if (problems.length === 2) {
+      return (
+        <td className="p-0" key={contestId + index.charAt(0)}>
+          <table>
+            <tbody>
+              <tr className="inside p-0" key={contestId + index}>
+                {problems.map((element) => renderProblem(element))}
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      );
+    }
+
+    return (
+      <td className="inside p-0" key={contestId + index}>
+        More than 4
+      </td>
+    );
+  };
 
   const contestCard = (contest) => {
     return (
